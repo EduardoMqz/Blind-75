54497a7995b2200d7e5bfc0bcf0e9b9383d5c4bb
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2024-11-02 18:15:50 -0600

    add most efficient

--- Dynamic Programming/16-Climbing Stars.cs
+++ Dynamic Programming/16-Climbing Stars.cs
@@ -38,5 +38,15 @@ public class SolutionXVI{
             return dp[n];
     }
 }
+/*
+if(n <= 2)
+    return n;
+int first = 1, second = 2;
+for(int i = 1; i <= n-2; i++){
+    int temp = first + second;
+    first = second;
+    second = temp
+}
+return second;
 
     
