45e262efcb80d3afa7fce5013b31c877e3494869
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2024-11-04 08:17:43 -0600

    fix

--- Dynamic Programming/18-Longest Increasing Subsequence.cs
+++ Dynamic Programming/18-Longest Increasing Subsequence.cs
@@ -16,14 +16,27 @@ Input: nums = [7,7,7,7,7,7,7]
 Output: 1
 */
 using System;
+using System.Runtime.InteropServices;
 
 namespace Dynamic_Programming;
 
 public class SolutionXVIII{
-    public int LengthOfLIS(int[] nums) {
-        List<int> dp = new List<int>();
-
-        return 0;
+    public int LengthOfList(int[] nums) {
+        int len = nums.Length;
+        if(len == 1) return 1;
+        int[] dp = new int[len];
+        dp[0] = 1;
+        int ans = 0;
+        for (int i = 1; i < len; i++){
+            int size = 0;
+            for (int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    size = Math.Max(size, dp[j]);
+                } 
+            }
+            dp[i] = 1 + size;
+            ans = Math.Max(ans, dp[i]);
+        }
+        return ans;
     }
-
 }
