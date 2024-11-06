041a9625469acdf381605acbd4672330d9cc1631
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2024-11-06 17:42:15 -0600

    efficient

--- Dynamic Programming/22-House Robber.cs
+++ Dynamic Programming/22-House Robber.cs
@@ -35,3 +35,15 @@ public class SolutionXXII{
         return dp[nums.Length - 1];
     }
 }
+
+/*
+if(num.length == 1) return nums[0];
+int prev1 = nums[0];
+int prev2 = Math.max(nums[0], nums[1]);
+for(int i = 2; i < nums.length; i++){
+ int curr = Math.max(nums[i] + prev1, prev2);
+ prev1 = prev2;
+ prev2 = curr;
+}
+return prev2;
+*/
